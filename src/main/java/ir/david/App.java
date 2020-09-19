0d980a79ca5003b967587698ae63d22d0f2dfac6
Davood Akbari <daak1365@gmail.com>
2020-09-19 15:34:45 +0430

    آماده ی اولیه ی spring bach

---
+++ src/main/java/ir/david/App.java
@@ -0,0 +1,41 @@
+package ir.david;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Davood Akbari - 1399
+ * daak1365@gmail.com
+ * daak1365@yahoo.com
+ * 09125188694
+ */
+public class App {
+    public static void main(String[] args)
+            throws JobParametersInvalidException, JobExecutionAlreadyRunningException,
+            JobRestartException, JobInstanceAlreadyCompleteException {
+
+        // System.out.println("hello");
+        String[] springConfig  =  {"jobConfig.xml"};
+
+        // Creating the application context object
+        ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+        // Creating the job launcher
+        JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+
+        // Creating the job
+        Job job = (Job) context.getBean("helloWorldJob");
+
+        // Executing the JOB
+        JobExecution execution = jobLauncher.run(job, new JobParameters());
+        System.out.println("Exit Status : " + execution.getStatus());
+    }
+}
---
+++ src/main/java/ir/david/MyTasklet.java
@@ -0,0 +1,21 @@
+package ir.david;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+/**
+ * @author Davood Akbari - 1399
+ * daak1365@gmail.com
+ * daak1365@yahoo.com
+ * 09125188694
+ */
+public class MyTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
+        System.out.println("Hello This is a sample example of spring batch");
+        return RepeatStatus.FINISHED;
+    }
+}
